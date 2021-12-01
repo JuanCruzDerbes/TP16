96dd53c9623e91d8d7dca7ebc442a3d2b4d11d36
JuanCruzDerbes <95315604+JuanCruzDerbes@users.noreply.github.com>
2021-11-30 21:04:53 -0300

    Add files via upload

---
+++ Trabajos-Node.js-main/2T TP Nº 15 Node.js - 03 Cifrado César/cifrar.js
@@ -0,0 +1,91 @@
+const http = require("http");
+const url = require("url");
+const fs = require("fs");
+const querystring = require("querystring");
+
+const mime = {
+    html: "text/html",
+    css: "text/css",
+    jpg: "image/jpg",
+    ico: "image/x-icon",
+    mp3: "audio/mpeg3",
+    mp4: "video/mp4",
+};
+
+const servidor = http.createServer((pedido, respuesta) => {
+    const objetourl = url.parse(pedido.url);
+    let camino = "public" + objetourl.pathname;
+    if (camino == "public/") camino = "public/index.html";
+    encaminar(pedido, respuesta, camino);
+});
+
+servidor.listen(8888);
+
+function encaminar(pedido, respuesta, camino) {
+    console.log(camino);
+    switch (camino) {
+        case "public/recuperardatos":
+            {
+                recuperar(pedido, respuesta);
+                break;
+            }
+        default:
+            {
+                fs.stat(camino, (error) => {
+                    if (!error) {
+                        fs.readFile(camino, (error, contenido) => {
+                            if (error) {
+                                respuesta.writeHead(500, { "Content-Type": "text/plain" });
+                                respuesta.write("Error interno");
+                                respuesta.end();
+                            } else {
+                                const vec = camino.split(".");
+                                const extension = vec[vec.length - 1];
+                                const mimearchivo = mime[extension];
+                                respuesta.writeHead(200, { "Content-Type": mimearchivo });
+                                respuesta.write(contenido);
+                                respuesta.end();
+                            }
+                        });
+                    } else {
+                        respuesta.writeHead(404, { "Content-Type": "text/html" });
+                        respuesta.write(
+                            "<!doctype html><html><head></head><body>Recurso inexistente</body></html>"
+                        );
+                        respuesta.end();
+                    }
+                });
+            }
+    }
+}
+
+function recuperar(pedido, respuesta) {
+    let info = "";
+    pedido.on("data", (datosparciales) => {
+        info += datosparciales;
+    });
+    pedido.on("end", () => {
+        const formulario = querystring.parse(info);
+        respuesta.writeHead(200, { "Content-Type": "text/html" });
+        const pagina =
+            `<!doctype html><html><head><link rel="stylesheet" href="style.css" /></head><body><div class="div">` +
+            cifrar(formulario["string"], parseInt(formulario["num"])) +
+            `</div></body></html>`;
+        respuesta.end(pagina);
+        console.log(cifrar(parseInt(formulario["num"])));
+    });
+}
+
+function cifrar(string, n) {
+
+    let string2 = "";
+    for (x = 0; x < string.length; x++) {
+        let letra2 = String.fromCharCode(string.charCodeAt(x) + n);
+        string2 += letra2;
+    }
+    console.log(string2);
+    console.log();
+    return string2;
+}
+
+console.log("Servidor web iniciado");
